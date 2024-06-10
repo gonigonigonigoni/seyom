282ce0bf6848752e3d9f274d81debf8eecaa5845
gonigonigonigoni goni <see802@naver.com>
2024-06-10 06:27:01 +0000

    햄슷타

--- 프로젝트폴더/script2.js
+++ 프로젝트폴더/script2.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const hamster = document.getElementById('hamster');
+    hamster.addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
+
     const hearts = document.querySelectorAll('.heart');
     hearts.forEach((heart, index) => {
         heart.style.animationDelay = `${index * 1}s`;
